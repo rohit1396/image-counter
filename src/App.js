c1d5897a02e6c15b8e9b6ad6f586947b7fb7358c
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-05-19 21:11:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pixel, setPixel] = useState(300);
+
+  const increaseCount = () => {
+    setPixel(pixel + 2);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80"
+        alt="random_img"
+        onClick={increaseCount}
+        style={{
+          height: `${pixel}px`,
+          width: `${pixel}px`,
+          objectFit: "contain",
+        }}
+      />
     </div>
   );
 }
